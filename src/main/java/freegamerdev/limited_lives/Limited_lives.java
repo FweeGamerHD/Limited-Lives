171a37626aa5af70228d2d73e5f04359c66e7b9c
FweeGamerHD <freegamerhd12@gmail.com>
2024-08-13 20:08:42 +0200

    initial commit

---
+++ src/main/java/freegamerdev/limited_lives/Limited_lives.java
@@ -0,0 +1,10 @@
+package freegamerdev.limited_lives;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Limited_lives implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
