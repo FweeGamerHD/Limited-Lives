dc0b60a82aaa5362d8a2eb61dee4c5c8bcadc3ee
FweeGamerHD <freegamerhd12@gmail.com>
2024-08-13 22:42:15 +0200

    registered event listener for respawning

--- src/main/java/freegamerdev/limited_lives/Limited_lives.java
+++ src/main/java/freegamerdev/limited_lives/Limited_lives.java
@@ -1,10 +1,21 @@
 package freegamerdev.limited_lives;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class Limited_lives implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        // Register the event listener for player respawn
+        ServerPlayerEvents.AFTER_RESPAWN.register(this::onPlayerRespawn);
+    }
+
+    // This method runs every time a player respawns on the server
+    private void onPlayerRespawn(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
+        if (!alive) {
+
+        }
     }
 }
