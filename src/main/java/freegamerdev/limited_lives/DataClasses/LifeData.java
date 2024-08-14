e588a38dc979f6d97dd5883cc15bfc705ae1cb93
FweeGamerHD <freegamerhd12@gmail.com>
2024-08-14 23:44:11 +0200

    additional methods required for logic and renamed some to make sense with how they get used

--- src/main/java/freegamerdev/limited_lives/DataClasses/LifeData.java
+++ src/main/java/freegamerdev/limited_lives/DataClasses/LifeData.java
@@ -4,17 +4,29 @@ import java.util.HashMap;
 
 public class LifeData {
 
-    private HashMap<String, Integer> playerLives;
+    private HashMap<String, Integer> playerDeaths;
 
     public LifeData() {
-        playerLives = new HashMap<>();
+        playerDeaths = new HashMap<>();
     }
 
-    public HashMap<String, Integer> getPlayerLives() {
-        return playerLives;
+    public void addPlayer(String uuid, int deaths) {
+        playerDeaths.put(uuid, deaths);
     }
 
-    public void setPlayerLives(HashMap<String, Integer> playerLives) {
-        this.playerLives = playerLives;
+    public int getDeathsFromPlayer(String uuid) {
+        return playerDeaths.get(uuid);
+    }
+
+    public void setDeathsForPlayer(String uuid, int deaths) {
+        playerDeaths.put(uuid, deaths);
+    }
+
+    public HashMap<String, Integer> getPlayerDeaths() {
+        return playerDeaths;
+    }
+
+    public void setPlayerDeaths(HashMap<String, Integer> playerDeaths) {
+        this.playerDeaths = playerDeaths;
     }
 }
